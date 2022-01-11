9508b8f7fa14ac321659c821b4539736b55b55b7
Max <maxskojig@gmail.com>
2022-01-12 00:01:21 +0100

    test

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,9 +7,8 @@ module.exports = {
         "eslint:recommended",
         "plugin:react/recommended",
         "plugin:@typescript-eslint/recommended",
-        "prettier",
-        "prettier/@typescript-eslint",
-        "prettier/react"
+        "plugin:prettier/recommended",
+        "prettier"
     ],
     parser: "@typescript-eslint/parser",
     parserOptions: {
